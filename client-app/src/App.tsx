ece3e3249a5c5f6ac29ca996e27b25d3f7dd7626
Kimeshnee <kimeshnee@utility-systems.co.za>
2023-08-15 16:16:01 +0200

    CORS

---
+++ client-app/src/App.tsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios';
+
+function App() {
+  const [activites, setActivities] = useState([]);
+
+  useEffect(() => {
+
+    axios.get('http://localhost:5000/api/activities')
+    .then(response => {
+      console.log(response);
+      setActivities(response.data);
+    })
+  }, []) //[] only do this one and not an infinite loop
+
+
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      <ul>
+        {activites.map((activity: any) => (
+          <li key = {activity.id}>
+            {activity.title}
+          </li>
+        ))}
+      </ul>
+      </header>
+    </div>
+  );
+}
+
+export default App;
